c2325e2c5f3647284b3bb7e2ec458db71d55239c
B-Lucas365 <lucasrenan365@outlook.com>
2022-03-10 10:23:01 -0300

    first commit project complete

---
+++ script.js
@@ -0,0 +1,9 @@
+function imgSlider(anything){
+    document.querySelector('.pepsi').src = anything
+    
+}
+
+function chengeBgColor(color){
+    const  container = document.querySelector('.container')
+    container.style.backgroundColor = color
+}
